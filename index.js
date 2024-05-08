e8c1895d01c1696f7d3536fd5d4a690a70af4950
tuoi8888 <juan251961pun0suy@hotmail.com>
2024-05-08 14:00:46 +0700

    start project

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-function removeDuplicates(nums) {
-  let index = 0;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== nums[index]) {
-      index++;
-      nums[index] = nums[i];
+function solveSudoku(board) {
+  solve(board);
+  function solve(board) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === ".") {
+          for (let num = 1; num <= 9; num++) {
+            const char = num.toString();
+            if (isValid(board, i, j, char)) {
+              board[i][j] = char;
+              if (solve(board)) return true;
+              board[i][j] = ".";
+            }
+          }
+          return false;
+        }
+      }
     }
+    return true;
   }
-  return index + 1;
+  function isValid(board, row, col, char) {
+    for (let i = 0; i < 9; i++) {
+      if (
+        board[row][i] === char ||
+        board[i][col] === char ||
+        board[3 * Math.floor(row / 3) + Math.floor(i / 3)][
+          3 * Math.floor(col / 3) + (i % 3)
+        ] === char
+      )
+        return false;
+    }
+    return true;
+  }
 }

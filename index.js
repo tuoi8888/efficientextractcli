a6a43a1a66b808fc3e208fac150e332d64e57649
tuoi8888 <juan251961pun0suy@hotmail.com>
2024-05-09 07:23:47 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-function solveSudoku(board) {
-  solve(board);
-  function solve(board) {
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        if (board[i][j] === ".") {
-          for (let num = 1; num <= 9; num++) {
-            const char = num.toString();
-            if (isValid(board, i, j, char)) {
-              board[i][j] = char;
-              if (solve(board)) return true;
-              board[i][j] = ".";
-            }
-          }
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-  function isValid(board, row, col, char) {
-    for (let i = 0; i < 9; i++) {
-      if (
-        board[row][i] === char ||
-        board[i][col] === char ||
-        board[3 * Math.floor(row / 3) + Math.floor(i / 3)][
-          3 * Math.floor(col / 3) + (i % 3)
-        ] === char
-      )
-        return false;
-    }
-    return true;
-  }
+function LRUCache(capacity) {
+  this.capacity = capacity;
+  this.cache = new Map();
 }
+LRUCache.prototype.get = function (key) {
+  if (!this.cache.has(key)) return -1;
+  const value = this.cache.get(key);
+  this.cache.delete(key);
+  this.cache.set(key, value);
+  return value;
+};
+LRUCache.prototype.put = function (key, value) {
+  if (this.cache.has(key)) this.cache.delete(key);
+  if (this.cache.size === this.capacity) {
+    const firstKey = this.cache.keys().next().value;
+    this.cache.delete(firstKey);
+  }
+  this.cache.set(key, value);
+};

f963cdfd4772ccf6f776e8121d6b60996f43e716
tuoi8888 <juan251961pun0suy@hotmail.com>
2024-05-11 07:08:45 +0700

    hello world

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-function reorderList(head) {
-  if (!head || !head.next) return;
-  let slow = head;
-  let fast = head;
-  while (fast.next && fast.next.next) {
-    slow = slow.next;
-    fast = fast.next.next;
+const fibonacciRecursive = (n) => {
+  if (n <= 1) {
+    return n;
   }
-  let prev = null;
-  let curr = slow.next;
-  slow.next = null;
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
-  }
-  let first = head;
-  let second = prev;
-  while (second.next) {
-    const next = first.next;
-    first.next = second;
-    first = next;
-    const next2 = second.next;
-    second.next = first;
-    second = next2;
-  }
-}
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+};
